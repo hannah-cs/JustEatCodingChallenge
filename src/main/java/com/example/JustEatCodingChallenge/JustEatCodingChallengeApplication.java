4aa74c2f67be068ac4404be42975b1e59d3b0d5f
hcs <hannahemcs@gmail.com>
2024-04-22 13:02:46 +0200

    Import cleanup

--- src/main/java/com/example/JustEatCodingChallenge/JustEatCodingChallengeApplication.java
+++ src/main/java/com/example/JustEatCodingChallenge/JustEatCodingChallengeApplication.java
@@ -1,17 +1,12 @@
 package com.example.JustEatCodingChallenge;
 
 import com.example.JustEatCodingChallenge.UI.UserInterface;
-import com.example.JustEatCodingChallenge.models.Restaurant;
 import com.example.JustEatCodingChallenge.service.APIService;
-import org.apache.catalina.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.Scanner;
 
 @SpringBootApplication
 public class JustEatCodingChallengeApplication {
