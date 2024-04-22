be43501e35e84c73260207ba552205f56a844921
hcs <hannahemcs@gmail.com>
2024-04-22 11:07:42 +0200

    Initial commit

---
+++ src/main/java/com/example/JustEatCodingChallenge/JustEatCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.JustEatCodingChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustEatCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustEatCodingChallengeApplication.class, args);
+	}
+
+}
